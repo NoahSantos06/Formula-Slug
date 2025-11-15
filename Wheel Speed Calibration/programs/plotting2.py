60a0894716d5694a426dc987f6453a766615f8e0
Noah Santos <nosantos@ucsc.edu>
2025-11-15 00:04:14 +0000

    changed plotting2.py

--- Wheel Speed Calibration/programs/plotting2.py
+++ Wheel Speed Calibration/programs/plotting2.py
@@ -2,21 +2,45 @@ import matplotlib.pyplot as plt
 import polars as pl
 import numpy as np
 
-df = pl.read_parquet("Wheel Speed Calibration/parquets/1109-4_53_filtered.parquet")
+df_1108_11_54 = pl.read_parquet("Wheel Speed Calibration/parquets/1108-11_54_filtered.parquet")
+df_1109_4_26 = pl.read_parquet("Wheel Speed Calibration/parquets/1109-4_26_filtered.parquet")
+df_1109_4_30 = pl.read_parquet("Wheel Speed Calibration/parquets/1109-4_30_filtered.parquet")
+df_1109_4_53 = pl.read_parquet("Wheel Speed Calibration/parquets/1109-4_53_filtered.parquet")
 
-back_left_wheel_speed = df["TPERIPH_BL_DATA_WHEELSPEED"]
-back_right_wheel_speed = df["TPERIPH_BR_DATA_WHEELSPEED"]
-front_left_wheel_speed = df["TPERIPH_FL_DATA_WHEELSPEED"]
-front_right_wheel_speed = df["TPERIPH_FR_DATA_WHEELSPEED"]
-gps_speed = df["VDM_GPS_SPEED"]
-motor_rpm = df["SME_TRQSPD_Speed"]
+
+bl_wheel_speed = "TPERIPH_BL_DATA_WHEELSPEED"
+br_wheel_speed = "TPERIPH_BR_DATA_WHEELSPEED"
+fr_wheel_speed = "TPERIPH_FR_DATA_WHEELSPEED"
+fl_wheel_speed = "TPERIPH_FL_DATA_WHEELSPEED"
+gps_speed = "VDM_GPS_SPEED"
+motor_rpm = "SME_TRQSPD_Speed"
 
 fig = plt.figure()
 
-ax2 = fig.add_subplot(111)
-ax2.plot(gps_speed * (63360/60) * (1/(16*np.pi)), alpha=0.6, label="gps speed -> rpm")
-ax2.plot(front_left_wheel_speed * 0.8452494744062437, alpha=0.6, label="fl wheel speed -> rpm")
-ax2.set_title("gps speed -> rpm vs fl wheel speed -> rpm")
+wheel_speed_coeff = 0.8452494744062437
+
+ax1 = fig.add_subplot(221)
+ax1.plot(df_1108_11_54[fl_wheel_speed] * wheel_speed_coeff, label="fl wheel speed to rpm")
+ax1.plot(df_1108_11_54[motor_rpm] * (12/41), label="motor rpm to wheel rpm")
+ax1.set_title("wheel speed conversion vs motor rpm conversion for 1108-11_54")
+ax1.legend()
+
+ax2 = fig.add_subplot(222)
+ax2.plot(df_1109_4_26[fl_wheel_speed] * wheel_speed_coeff, label="fl wheel speed to rpm")
+ax2.plot(df_1109_4_26[motor_rpm] * (12/41), label="motor rpm to wheel rpm")
+ax2.set_title("wheel speed conversion vs motor rpm conversion for 1109-4_26")
 ax2.legend()
+
+ax3 = fig.add_subplot(223)
+ax3.plot(df_1109_4_30[fl_wheel_speed] * wheel_speed_coeff, label="fl wheel speed to rpm")
+ax3.plot(df_1109_4_30[motor_rpm] * (12/41), label="motor rpm to wheel rpm")
+ax3.set_title("wheel speed conversion vs motor rpm conversion for 1109-4_30")
+ax3.legend()
+
+ax4 = fig.add_subplot(224)
+ax4.plot(df_1109_4_53[fl_wheel_speed] * wheel_speed_coeff, label="fl wheel speed to rpm")
+ax4.plot(df_1109_4_53[motor_rpm] * (12/41), label="motor rpm to wheel rpm")
+ax4.set_title("wheel speed conversion vs motor rpm conversion for 1109-4_53")
+ax4.legend()
 
 plt.show()
